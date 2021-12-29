97b56a51a085c64bb6cee16ee53dabe376894b75
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-29 14:54:19 -0300

    modularization app with sub-modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CharacterComponent } from './components/characters/character/character.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FormSearchComponent } from './shared/components/form-search/form-search.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterComponent,
     HeaderComponent,
     FormSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Character } from '@shared/interfaces/character.interface';
+import { environment } from '@environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharacterService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // service http
+  searchCharacters(query = '', page = 1) {
+    const filter = `${environment.baseUrlAPI}/?name=${query}&page=${page}`;
+    return this.http.get<Character[]>(filter);
+  }
+
+  getDetails(id: number) {
+    return this.http.get<Character>(`${environment.baseUrlAPI}/${id}`);
+  }
 }
